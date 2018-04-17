093b52cf2bf588ccfc72f4f10731d5902ea8d0c9
Jordan Brooks <jordanbrooks@MacBook-Pro-68.local>
2018-04-17 14:41:48 +0100

    update airport js

--- airplane_app/src/Airport.js
+++ airplane_app/src/Airport.js
@@ -1,6 +1,6 @@
 function Airport() {
    this.hangar = [],
-   this.max_capacity = 100,
+   this.max_capacity = 1,
    this.weather = 'sunny';
 }
 
@@ -9,7 +9,12 @@ Airport.prototype.setWeather = function(condition){
 };
 
 Airport.prototype.add_plane = function(plane) {
+  if(this.isFull()) throw new Error('airport is full');
   this.hangar.push(plane);
+};
+
+Airport.prototype.isFull = function() {
+  return this.hangar.length >= this.max_capacity;
 };
 
 Airport.prototype.remove_plane = function() {
