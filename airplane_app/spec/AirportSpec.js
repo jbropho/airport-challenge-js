1e084f74ccbb5732964885973faa314f9aa5b292
Sam Niechcial <samniechcial@gmail.com>
2018-04-17 16:17:48 +0100

    Weather randomised and weather stubbed for all unit tests

--- airplane_app/spec/AirportSpec.js
+++ airplane_app/spec/AirportSpec.js
@@ -1,35 +1,37 @@
 describe('Airport', function() {
   var gatwick = new Airport();
+  var heathrow = new Airport();
 
   beforeEach(function(){
     gatwick.hangar = [];
+    spyOn(gatwick, "getWeather").and.returnValue('sunny');
   });
 
-  describe('add_plane', function() {
+  describe('addPlane', function() {
     it('should add a plane to the hangar', function() {
-       gatwick.add_plane('plane');
+       gatwick.addPlane('plane');
        expect(gatwick.hangar.length).toEqual(1);
     });
   });
 
     it('should raise an error when adding to full hangar', function(){
-      gatwick.add_plane('plane');
+      gatwick.addPlane('plane');
       expect(function() {
-        gatwick.add_plane('plane2');
+        gatwick.addPlane('plane2');
       }).toThrowError('airport is full');
     }); 
 
     it('should raise an error when attempting to add when weather is stormy', function(){
-      spyOn(gatwick, "getWeather").and.returnValue('stormy');
+      spyOn(heathrow, "getWeather").and.returnValue('stormy');
       expect(function() {
-        gatwick.add_plane('plane');
+        heathrow.addPlane('plane');
       }).toThrowError('weather unsuitable for landing');
     })
 
-  describe('remove_plane', function() {
+  describe('removePlane', function() {
     it('should remove a plane from the hangar', function() {
-      gatwick.add_plane('plane');
-      gatwick.remove_plane();
+      gatwick.addPlane('plane');
+      gatwick.removePlane();
       expect(gatwick.hangar.length).toEqual(0);
     });
   });
--- airplane_app/src/Airport.js
+++ airplane_app/src/Airport.js
@@ -1,14 +1,15 @@
 function Airport() {
    this.hangar = [],
-   this.max_capacity = 1,
-   this.weather = 'sunny';
+   this.max_capacity = 1
 }
 
-Airport.prototype.getWeather = function(){
-  return this.weather;
+Airport.prototype.getWeather = function() {
+  var weatherArray = ['sunny', 'stormy', 'sunny', 'sunny']
+  var number = Math.floor(Math.random() * weatherArray.length)
+  return weatherArray[number]
 };
 
-Airport.prototype.add_plane = function(plane) {
+Airport.prototype.addPlane = function(plane) {
   if(this.isFull()) throw new Error('airport is full');
   if(this.getWeather() === 'stormy') throw new Error('weather unsuitable for landing')
   this.hangar.push(plane);
@@ -18,6 +19,6 @@ Airport.prototype.isFull = function() {
   return this.hangar.length >= this.max_capacity;
 };
 
-Airport.prototype.remove_plane = function() {
+Airport.prototype.removePlane = function() {
   this.hangar.pop();
 };
